13176b22ae1ae475a18971bd964857f21682bc5b
Ayush Shete <ayushete02@gmail.com>
2022-10-18 01:30:42 +0530

    Update in BOT

--- twitter-bot.py
+++ twitter-bot.py
@@ -1,29 +1,34 @@
 import tweepy, time, sys
 import config
 
-
 def twitter_bot():
-    # file from which to read tweets in
-    argfile = str(sys.argv[1])
-
-    # number of seconds to wait until next tweet
-    seconds = int(sys.argv[2])
-
     # establish connection to twitter api
     auth = tweepy.OAuthHandler(config.CONSUMER_KEY, config.CONSUMER_SECRET)
     auth.set_access_token(config.ACCESS_TOKEN, config.ACCESS_SECRET)
     api = tweepy.API(auth)
 
+    temp = '' 
+    tweets = []
+
     # read in the tweets
-    with open(argfile, "r") as file:
+    with open('example.txt', "r") as file:
         content = file.read()
-        tweets = content.split("§§§")
+    
+    for char in content:
+        if char == '~':
+            tweets.append(temp)
+            temp = ''
+        else:
+            temp += char
+    tweets.append(temp)
+    print(tweets)        
 
     # publish the tweets
     for tweet in tweets:
         tweet = tweet.strip()
+        print(tweet)
         api.update_status(tweet)
-        time.sleep(seconds)  # update status every 15 minutes
+        time.sleep(15*60)  # update status every 15 minutes
 
 
 if __name__ == "__main__":
