7c49cba9d742871fc48de26ad884f788001d2cc5
Sanskar <mishrasanskar@gmail.com>
2022-10-18 17:33:50 +0530

    added files to folder

--- config.py
+++
@@ -1,5 +0,0 @@
-# twitter api credentials - https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api
-CONSUMER_KEY = ""
-CONSUMER_SECRET = ""
-ACCESS_TOKEN = ""
-ACCESS_SECRET = ""
