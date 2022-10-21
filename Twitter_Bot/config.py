73880bd9c23f0d1bf8542a75466d693cf5dbf1e3
Sanskar Mishra <95489547+whythoSanskar@users.noreply.github.com>
2022-10-21 23:30:15 +0530

    Update config.py

--- Twitter_Bot/config.py
+++ Twitter_Bot/config.py
@@ -1,5 +1,5 @@
 # twitter api credentials - https://developer.twitter.com/en/docs/twitter-api/getting-started/getting-access-to-the-twitter-api
-CONSUMER_KEY = "LwKeoe3poxgQ2HCpPiN5f9bNs"
-CONSUMER_SECRET = "iUJvPXAJvhVhAvBaqBMI1rNaY2RrkyPiQqZ4uuS6pFLGGKX3G7"
-ACCESS_TOKEN = "1512353783690723332-OPH4z0QbBxcHCiY3fej9CEF4rzkwdE"
-ACCESS_SECRET = "bOSksfRVmZk5WvsQL2lC9xXLG7Qluccp0TyoRXCZsr5PQ"
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
+ACCESS_TOKEN = ""
+ACCESS_SECRET = ""
